6a4f3f3ff5c1cb5d246922993dc11b335e01f23f
Yani Asari <yani.iman1@gmail.com>
2020-09-02 03:05:35 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -20,8 +20,8 @@ class Scraper
     course.title = post.css("h2").text
     course.schedule = post.css(".date").text
     course.description = post.css("p").text
+      end
     end
-  end
       # doc.css(".post").each do |post|
       # course = Course.new
       # course.title = post.css("h2").text
